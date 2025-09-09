d2a7261ba7829a4b9f578cb53d926a7bfd5faee3
pinzos <soporte.pinzos@gmail.com>
2025-09-09 12:45:42 -0500

    Update supabaseClient.js

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-// ⚡️ Reemplaza con tus credenciales del .env
-const supabaseUrl = https://esnfwxitclmbzdxuevwg.supabase.co
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzbmZ3eGl0Y2xtYnpkeHVldndnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNzcyMDIsImV4cCI6MjA3Mjk1MzIwMn0.7B55c2XZQeQKwsLeKcg-1THeH4GogccAVX2nQnflfEQ
+const supabaseUrl = "https://esnfwxitclmbzdxuevwg.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzbmZ3eGl0Y2xtYnpkeHVldndnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNzcyMDIsImV4cCI6MjA3Mjk1MzIwMn0.7B55c2XZQeQKwsLeKcg-1THeH4GogccAVX2nQnflfEQ";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
