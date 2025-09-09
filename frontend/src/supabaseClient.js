a93f0d7affe9c103c62abb18fd2128e207dff0ce
pinzos <soporte.pinzos@gmail.com>
2025-09-09 12:31:52 -0500

    Update supabaseClient.js

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // ⚡️ Reemplaza con tus credenciales del .env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
